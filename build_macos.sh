2de4b16329377c0fa1a141a65a27985389148670
Akash Levy <akash@silimate.com>
2025-06-07 00:23:26 -0700

    Fixups

--- build_macos.sh
+++ build_macos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 brew bundle install
 ./autogen.sh
-./configure CXXFLAGS="-std=c++11" --prefix=`brew --prefix` --enable-judy --enable-struct-pack "CFLAGS=-I`brew --prefix`/include -O2 -g" LDFLAGS=-L`brew --prefix`/lib  --no-create --no-recursion --with-tcl=`brew --prefix tcl-tk@8`/lib --with-tk=`brew --prefix tcl-tk@8`/lib
+./configure CXXFLAGS="-std=c++11" --prefix=`brew --prefix` --enable-judy --enable-struct-pack "CFLAGS=-I`brew --prefix`/include -O2 -g" LDFLAGS=-L`brew --prefix`/lib --with-tcl=`brew --prefix tcl-tk@8`/lib --with-tk=`brew --prefix tcl-tk@8`/lib
 make -j8
