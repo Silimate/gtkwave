9646b248dbe36f66c353c9c1c06a315a71cf7695
Akash Levy <akash@silimate.com>
2025-06-07 02:10:37 -0700

    Tentative TCL fixups

--- build_macos.sh
+++ build_macos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 brew bundle install
 ./autogen.sh
-./configure CXXFLAGS="-std=c++11" --enable-gtk3 --prefix=`brew --prefix` --enable-judy --enable-struct-pack "CFLAGS=-I`brew --prefix`/include -O2 -g" LDFLAGS=-L`brew --prefix`/lib --with-tcl=`brew --prefix tcl-tk@8`/lib --with-tk=`brew --prefix tcl-tk@8`/lib
+./configure CXXFLAGS="-std=c++11" --enable-stubs --prefix=`brew --prefix` --enable-judy --enable-struct-pack "CFLAGS=-I`brew --prefix`/include -O2 -g" LDFLAGS="-L`brew --prefix`/lib -L`brew --prefix tcl-tk@8`/lib -ltcl8.6 -ltk8.6" --with-tcl=`brew --prefix tcl-tk@8`/lib --with-tk=`brew --prefix tcl-tk@8`/lib
 make -j8
