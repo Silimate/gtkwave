0e4c15d85422ffa89423a7e3b4150b679b17af7e
Akash Levy <akash@silimate.com>
2025-06-07 20:46:23 -0700

    Fix command

--- build_macos.sh
+++ build_macos.sh
@@ -3,12 +3,11 @@ brew bundle install
 ./autogen.sh
 TCLTK_PREFIX=`brew --prefix tcl-tk@8`
 ./configure \
-    --enable-stubs \
     --enable-gtk3 \
     --prefix=`brew --prefix` \
     --with-tcl=$TCLTK_PREFIX/lib \
     --with-tk=$TCLTK_PREFIX/lib \
-    CFLAGS="-I`brew --prefix`/include -g -O3" \
+    CFLAGS="-I$TCLTK_PREFIX/include -I`brew --prefix`/include -g -O3" \
     CXXFLAGS="-std=c++11 -O3" \
-    LDFLAGS="-L`brew --prefix`/lib"
+    LDFLAGS="-L$TCLTK_PREFIX/lib -L`brew --prefix`/lib"
 make -j8
