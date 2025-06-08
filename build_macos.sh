fc83457aca6edcbf40cbaa470ecf0739884b458b
Akash Levy <akash@silimate.com>
2025-06-07 18:36:43 -0700

    Push

--- build_macos.sh
+++ build_macos.sh
@@ -7,7 +7,7 @@ TCLTK_PREFIX=`brew --prefix tcl-tk@8`
     --prefix=`brew --prefix` \
     --with-tcl=$TCLTK_PREFIX/lib \
     --with-tk=$TCLTK_PREFIX/lib \
-    "CFLAGS=-I`brew --prefix`/include -I$TCLTK_PREFIX/include -g" \
-    "CXXFLAGS=-std=c++11" \
-    "LDFLAGS=-L`brew --prefix`/lib -L$TCLTK_PREFIX/lib" \
+    CFLAGS="-I`brew --prefix`/include -g" \
+    CXXFLAGS="-std=c++11" \
+    LDFLAGS="-L`brew --prefix`/lib"
 make -j8
