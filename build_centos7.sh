8595b55a37d4790b17868bb0359850e41619bb20
Akash Levy <akash@silimate.com>
2026-01-25 14:41:35 -0800

    Cleanup

--- build_centos7.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo yum -y update
-sudo yum -y install meson gperf flex glib2-devel gcc gcc-c++ gtk2-devel \
-                    gobject-introspection-devel desktop-file-utils tcl \
-                    tk-devel bzip2-devel xorg-x11-server-Xvfb
-./autogen.sh
-./configure --enable-fsdb
-make -j8
--- build_ubuntu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y meson gperf flex desktop-file-utils libgtk-3-dev libbz2-dev libjudy-dev libgirepository1.0-dev
-./autogen.sh
-./configure --enable-fsdb
-make -j8
